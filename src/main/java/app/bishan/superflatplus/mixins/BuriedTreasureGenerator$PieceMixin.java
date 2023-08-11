ff3728bb6f27992abc1c364282690491900e355b
Kishan S Patel <bishan.batel@gmail.com>
2023-08-10 23:11:38 -0500

    Changed in favor of more verbose name

--- src/main/java/app/bishan/superflatplus/mixins/BuriedTreasureGenerator$PieceMixin.java
+++ src/main/java/app/bishan/superflatplus/mixins/BuriedTreasureGenerator$PieceMixin.java
@@ -8,23 +8,14 @@ import net.minecraft.structure.StructurePiece;
 import net.minecraft.structure.StructurePieceType;
 import net.minecraft.util.math.BlockBox;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkPos;
-import net.minecraft.util.math.random.Random;
-import net.minecraft.world.Heightmap;
 import net.minecraft.world.StructureWorldAccess;
-import net.minecraft.world.gen.StructureAccessor;
-import net.minecraft.world.gen.chunk.ChunkGenerator;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.Slice;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.*;
 
 @Mixin(BuriedTreasureGenerator.Piece.class)
-public abstract class BuriedTreasureGeneratorMixin extends StructurePiece {
+public abstract class BuriedTreasureGenerator$PieceMixin extends StructurePiece {
 
-	protected BuriedTreasureGeneratorMixin(StructurePieceType type, int length, BlockBox boundingBox) {
+	protected BuriedTreasureGenerator$PieceMixin(StructurePieceType type, int length, BlockBox boundingBox) {
 		super(type, length, boundingBox);
 	}
 
