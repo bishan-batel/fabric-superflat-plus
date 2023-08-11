7ff15e2d738656360ef50b097009415a707760cd
Kishan S Patel <bishan.batel@gmail.com>
2023-08-10 23:10:39 -0500

    Added client loader (unused for now)

--- src/main/java/app/bishan/superflatplus/client/SuperflatPlusClient.kt
+++ src/main/java/app/bishan/superflatplus/client/SuperflatPlusClient.kt
@@ -1,7 +1,10 @@
 package app.bishan.superflatplus.client
 
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.api.EnvType
+import net.fabricmc.api.Environment
 
+@Environment(EnvType.CLIENT)
 object SuperflatPlusClient : ClientModInitializer {
 	override fun onInitializeClient() {
 	}
