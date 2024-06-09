f4e1e4480de8b72bd820aa6b4e2aa0b235fc4444
Kishan S Patel <bishan.batel@protonmail.com>
2024-06-08 19:53:21 -0500

    Fixed desert temples

---
+++ src/main/java/app/bishan/superflatplus/mixins/BasicTemplateStructureMixin.java
@@ -0,0 +1,20 @@
+package app.bishan.superflatplus.mixins;
+
+import net.minecraft.world.gen.chunk.ChunkGenerator;
+import net.minecraft.world.gen.structure.BasicTempleStructure;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(BasicTempleStructure.class)
+public class BasicTemplateStructureMixin {
+    @Redirect(
+            method = "getStructurePosition",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/gen/chunk/ChunkGenerator;getSeaLevel()I"
+            ))
+    int huh(ChunkGenerator instance) {
+        return 0;
+    }
+}
